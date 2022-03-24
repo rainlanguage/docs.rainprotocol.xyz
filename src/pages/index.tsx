161369a35fddaa30d918d3581926eaf9106d5340
marcusnewton <marcus.newton@hotmail.com>
2022-03-24 15:53:19 +1100

    fix local search with missing Layout wrapper component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import  { Redirect } from 'react-router-dom';
+import React from "react";
+import { Redirect } from "react-router-dom";
+import Layout from "@theme/Layout";
 
 export default function Home() {
-  return <Redirect to='/intro' />;
+  return (
+    <Layout>
+      <Redirect to="/intro" />
+    </Layout>
+  );
 }
