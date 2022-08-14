65ce1e9d7443015f688af4ec5d6a901e3eb5b152
thedavidmeister <thedavidmeister@gmail.com>
2022-08-14 18:34:20 +0400

    blog post

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
             position: "left",
             label: "Docs",
           },
-          // {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: "https://github.com/beehive-innovation",
             label: "GitHub",
